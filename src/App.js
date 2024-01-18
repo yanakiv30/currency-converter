87e5e49edae0438cbd364c8be90ee1530ce0c65f
yanakiv30 <yanakiv30@gmail.com>
2024-01-18 23:28:27 +0200

    convert

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [amount, setAmount] = useState(1);
   const [fromCur, setFromCur] = useState('EUR');
   const [toCur, setToCur] = useState('USD');
+  const [converted, setConverted] = useState("");
+
+  useEffect(function() {
+    async function convert() {
+    const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`);
+    const data = await res.json();
+   setConverted(data.rates[toCur]);
+    }
+    convert();
+
+  }, [amount, fromCur, toCur])
 
   return (
     <div>
       <input type="text" value={amount} onChange={e => setAmount(+e.target.value)} />
-      <select value={fromCur} onChange={e => setFromCur(e.target.value)}>
+      <select value={fromCur} onChange={e => setFromCur(e.target.value)}>      
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={toCur} onChange={e => setToCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
-
-      {/* <input type="text" />
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p> */}
+      <p>{converted} {toCur}</p>      
     </div>
   );
 }
