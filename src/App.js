f0eacb764097487e686c5ec91151fe8b910eac5f
yanakiv30 <yanakiv30@gmail.com>
2024-01-18 22:59:46 +0200

    options

--- src/App.js
+++ src/App.js
@@ -1,3 +1,43 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import { useState } from "react";
+
 export default function App() {
-  return <div>Hello React</div>
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState('EUR');
+  const [toCur, setToCur] = useState('USD');
+
+  return (
+    <div>
+      <input type="text" value={amount} onChange={e => setAmount(+e.target.value)} />
+      <select value={fromCur} onChange={e => setFromCur(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+
+      {/* <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p> */}
+    </div>
+  );
 }
